543cba2382aee1c1241f28a1d8dd75a38b02a06f
ManaRebus <kolidentsevn@gmail.com>
2026-08-25 09:00:53 +0200

    Scoring + pipe free
    Add score label + handle the pipe scoring.
    Pipes now are freed when reach behind viewport

--- Level/level.gd
+++ Level/level.gd
@@ -2,27 +2,56 @@ extends Node2D
 
 const PIPE_PAIR := preload("res://Pipes/pipe_pare.tscn")
 
+@onready var bird: Bird = %FBird
 @onready var background: TextureRect = %Background
 @onready var ground: TextureRect = %Ground
-@onready var pipe_timer: Timer = %PipeTimer
-@onready var path_2d: Path2D = %Path2D
 @onready var pipe_y_spawn: PathFollow2D = %PipeYSpawn
+@onready var score_label: Label = %Score
 
+var score := 0
+var pipe_array: Array = []
+@export var spawn_interval := 2.0
+var pipe_timer = create_timer()
+
+@export_group("Background movement")
 @export var background_speed := 0.05
 @export var ground_speed := 0.2
 var background_offset := 0.0
 var ground_offset := 0.0
 
-func _ready() -> void:
-	pipe_timer.timeout.connect(spawn_pipes)
 
+#creates timer for pipes to spawn
+func create_timer() -> Timer:
+	var _timer := Timer.new()
+	_timer.autostart = true
+	_timer.one_shot = false
+	return _timer
+
+
+func _ready() -> void:
+	add_child(pipe_timer)
+	pipe_timer.start(spawn_interval)
+	pipe_timer.timeout.connect(spawn_pipes)
+	pipe_timer.timeout.connect(func() -> void:
+		print(str(pipe_array)))
+	
+
+# handels background a ground movement through shaders
 func _process(delta: float) -> void:
 	background_offset += background_speed * delta
 	ground_offset += ground_speed * delta
-	
 	background.material.set_shader_parameter("offset", background_offset)
 	ground.material.set_shader_parameter("offset", ground_offset)
 
+func _physics_process(_delta: float) -> void:
+	for pipe in pipe_array:
+		if pipe.position.x < bird.position.x and pipe.scored == false:
+			score += 1
+			pipe.scored = true
+			score_label.text = "Score: " + str(score)
+
+# spawning pipe-pairs along x and y:
+# x: outside of a screen; y: random position on a path2D
 func spawn_pipes() -> void:
 	var viewport_size := get_viewport_rect().size
 	var pipe_position := Vector2.ZERO
@@ -32,4 +61,18 @@ func spawn_pipes() -> void:
 	
 	var pipes = PIPE_PAIR.instantiate()
 	pipes.position = pipe_position
+
 	add_child(pipes)
+	pipe_array.append(pipes)
+	
+	pipes.pipe_deleted.connect(_on_pipe_deleted.bind(pipes))
+
+func _on_pipe_deleted(pipes) -> void:
+	pipe_array.erase(pipes)
+
+func scoring(score: int) -> void:
+	pass
+
+func game_over() -> void:
+	set_physics_process(false)
+	get_tree().reload_current_scene()
--- Pipes/pipe_pare.gd
+++ Pipes/pipe_pare.gd
@@ -2,13 +2,18 @@ class_name PipePair extends Node2D
 
 @export var pipe_speed := 200.0
 @export var gap_between_pipes := 80.0
+var scored := false
 
 @onready var lower_pipe: Area2D = %LowerPipe
 @onready var upper_pipe: Area2D = %UpperPipe
 
+signal pipe_deleted
 func _ready() -> void:
 	lower_pipe.position.y = gap_between_pipes / 2
 	upper_pipe.position.y = - gap_between_pipes / 2
 
 func _physics_process(delta: float) -> void:
 	position.x -= pipe_speed * delta
+	if position.x <= -100.0:
+		pipe_deleted.emit()
+		queue_free()
